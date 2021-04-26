32fc0c23490d2ffcd7d61e06f957c8c4f04b400d
Richard Annand <rj170590@gmail.com>
2021-04-26 23:07:38 +0100

    :art: Added addition of RPi.GPIO to setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="psb",
-    version='0.12.dev',
+    version='0.14.beta',
     packages=find_packages(),
     install_requires=[
         "AWSIoTPythonSDK",
         "inky",
         "font_fredoka_one",
         "Pillow",
-        "boto3"
+        "boto3",
+        "RPi.GPIO"
     ],
     entry_points={
         'console_scripts': [
