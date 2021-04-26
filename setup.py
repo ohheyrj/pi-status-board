deedc4554438233acb98b08d008db94f5795573b
Richard Annand <rj170590@gmail.com>
2021-04-26 23:46:18 +0100

    :art: Changes to setup.py
    Added additional metadata to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name="psb",
-    version='0.15.0',
+    author="Richard Annand",
+    author_email="rj170590@gmail.com",
+    url="https://github.com/rj175/pi-status-board",
+    project_urls={
+        "Bug Tracker": "https://github.com/rj175/pi-status-board/issues"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Home Automation",
+        "Topic :: Utilities",
+        "Natural Language :: English"
+    ],
+    version='0.16.0',
+    python_requires=">=3.6",
     packages=find_packages(),
     install_requires=[
         "AWSIoTPythonSDK",
